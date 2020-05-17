bb4437cf93abb316edd60671290a3b9ebdfb45e8
Sebastian Segura <segurasebastian7887@gmail.com>
2020-05-17 19:43:29 -0300

    Update db seeds

--- server/src/seeds/1-user.seed.ts
+++ server/src/seeds/1-user.seed.ts
@@ -1,14 +1,17 @@
 import { Seeder, Factory } from 'typeorm-seeding'
 import { Connection } from 'typeorm'
+import * as bcrypt from 'bcryptjs'
 import { Group } from '../groups/group.entity'
 import { User, RoleType } from '../users/user.entity'
 import { getGroupId } from '../utils'
 
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
+    const salt = bcrypt.genSaltSync(10)
+    const password = bcrypt.hashSync('admin1234', salt)
     await factory(User)().create({
       email: 'admin@iot.com',
-      password: 'admin1234',
+      password,
       role: RoleType.ADMIN,
       groupId: null
     })
